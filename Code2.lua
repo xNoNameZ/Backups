ecaa7080da0bed82f67da41a1f484b57a10b593b
xNoNameZ <pixtureshub@gmail.com>
2025-01-12 17:04:12 +0700

    Create Code2.lua

---
+++ Code2.lua
@@ -0,0 +1,24 @@
+local Code = {
+  "KITT_RESET",
+  "Sub2CaptainMaui",
+  "DEVSCOOKING",
+  "kittgaming",
+  "Sub2Fer999",
+  "Enyu_is_Pro",
+  "Magicbus",
+  "JCWK",
+  "Starcodeheo",
+  "Bluxxy",
+  "fudd10_v2",
+  "SUB2GAMERROBOT_EXP1",
+  "Sub2NoobMaster123",
+  "Sub2Daigrock",
+  "Axiore",
+  "TantaiGaming",
+  "StrawHatMaine",
+  "Sub2OfficialNoobie",
+  "Fudd10",
+  "Bignews",
+  "TheGreatAce"
+}
+return Code
